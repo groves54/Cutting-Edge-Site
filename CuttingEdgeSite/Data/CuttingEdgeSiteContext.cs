9e7547326c0515a62215a0d7f74e6b1364203448
groves54 <32249748+groves54@users.noreply.github.com>
2017-10-01 20:14:24 -0400

    Commit

---
+++ CuttingEdgeSite/Data/CuttingEdgeSiteContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace CuttingEdgeSite.Models
+{
+    public class CuttingEdgeSiteContext : DbContext
+    {
+        public CuttingEdgeSiteContext (DbContextOptions<CuttingEdgeSiteContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<CuttingEdgeSite.Models.Product> Product { get; set; }
+    }
+}
